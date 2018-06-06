bb2c67476bde59c81d097a3ca441d365984af131
Rajasekaran <proffraja@gmail.com>
2018-06-06 14:54:05 +0530

    Screen Shots Added and Manual Added

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,15 @@
 import java.io.File;
-
-class UserFileHandler{
-	File aFile;
-	boolean isReadable = false;
-	boolean isWriteable = false;
-	boolean isExists = false;
-	int length = 0;
-	public UserFileHandler(String path) {
-		aFile = new File(path);
-		this.isExists = aFile.exists();
-		this.isReadable = aFile.canRead();
-		this.isWriteable = aFile.canWrite();
-		this.length = (int) aFile.length();
-	}
-	public void fileDetails(){
-		if(isExists){
-			System.out.println("The File "+aFile.getName()+" is Available at:"+aFile.getParent());
-			if(isReadable && isWriteable)
-				System.out.println("File is Readable and Writeable");
-			else if(isReadable)
-				System.out.println("File is Only Readable");
-			else if(isWriteable)
-				System.out.println("File is Only Writeable");
-			System.out.println("Total length of the file is :"+this.length+" bytes");
-		}
-		else
-			System.out.println("File does not exists ");
-	}
-}
+import java.util.Scanner;
+import com.raja.oopslab.files.*;
 public class Main {
 
 	public static void main(String[] args) {
-		new UserFileHandler("/home/raja/test.txt").fileDetails();
+		String file_path = null;
+		Scanner input = new Scanner(System.in);
+		System.out.println("File Handler");
+		System.out.println("************");
+		System.out.println("Enter the file path");
+		file_path = input.next();
+		new UserFileHandler(file_path).fileDetails();
 	}
 }
